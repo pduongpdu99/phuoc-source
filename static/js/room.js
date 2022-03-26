a3ef131f00195c2bdbce503f7bf43d23620eb493
Duong <dct.web.developer@gmail.com>
2022-03-26 14:57:41 +0700

    update source - switch room

--- static/js/room.js
+++ static/js/room.js
@@ -5,6 +5,7 @@ import {
     RoomProvider,
 } from '/providers/index.js';
 
+// validation
 import {
     isNotEmpty,
     isEmail,
@@ -12,6 +13,7 @@ import {
     isDate
 } from './validation.js';
 
+// DANTOC constants
 import {
     DANTOC
 } from '/utils/constant.js';
@@ -547,10 +549,208 @@ function initialize(registerForm) {
 // ====================================================================================================================================================
 // ====================================================================================================================================================
 
-// tạo form chuyển phòng cho user
-function roomUpdateInit(userId, roomId) {
-    console.log(
-        userId,
-        roomId
-    )
+/**
+ * switch form html init
+ * @param {Room} roomModel 
+ * @returns 
+ */
+const initSwitchFormHTML = (roomModel) => {
+    console.log(roomModel);
+    return `
+    <div class="bg-dark"></div>
+    <switch-form user="user_id" room="room_id" class="light">
+       <div class="cont">
+          <div class="form sign-in">
+             <h2>Room switch</h2>
+             <label>
+                <span>Current</span>
+                <input type="text" id="switch-form-current" placeholder="101" value="${roomModel.name}" readonly/>
+             </label>
+             <label>
+                <span>To</span>
+                <div class="autocomplete">
+                   <input type="text" id="switch-form-to" placeholder="101"/>
+                </div>
+             </label>
+             <button type="button" class="submit" id="switch-submit">Switch</button>
+             <button type="button" class="fb-btn" id="switch-cancel">Cancel</button>
+          </div>
+       </div>
+    </switch-form>
+    `;
+}
+
+/**
+ * tạo form chuyển phòng cho user
+ * @param {User} userId 
+ * @param {string} roomId 
+ */
+async function roomUpdateInit(user, roomId) {
+    const room = await RoomProvider.findById(roomId);
+
+    // render inner html into init-switch-form index
+    $('#init-switch-form').html(initSwitchFormHTML(room))
+
+    // khởi tạo switch form 
+    switchFormInit(roomId);
+
+    // assign event function cho button trong switch form 
+    document.getElementById('switch-submit').onclick = function () {
+        const roomdIdNeedToChange = document.getElementById('switch-form-to').getAttribute('data-id');
+
+        // check null
+        if (roomdIdNeedToChange && roomdIdNeedToChange.length > 0) {
+            onSwitchFormSubmit(
+                // thông tin người dùng hiện tại
+                user,
+
+                // dữ liệu data-id
+                roomdIdNeedToChange
+            );
+        } else alert("Dữ liệu không hợp lệ/ Dữ liệu không được rỗng")
+
+    };
+
+    // assign event function cho cancel button switch form 
+    document.getElementById('switch-cancel').onclick = onSwitchFormCancel;
+}
+
+/**
+ * on switch form submit
+ * @param {User} user
+ */
+function onSwitchFormSubmit(user, roomIdNeedToChanged) {
+    loadingCall();
+    user.roomId = roomIdNeedToChanged;
+
+    // use update by id api to room id change
+    UserProvider.updateById(user).then(userUpdated => {
+        // alert('Room id chanhed');
+        $('body').loadingModal('destroy');
+        onSwitchFormCancel();
+    });
+}
+
+/**
+ * on switch form cancel
+ */
+function onSwitchFormCancel() {
+    const switchForm = document.getElementById('init-switch-form');
+    switchForm.innerHTML = ``;
+}
+
+/**
+ * auto complete
+ * @param {*} inp 
+ * @param {Room[]} arr 
+ */
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+        var a, b, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+
+        /*for each item in the array...*/
+        arr.forEach(index => {
+            /*check if the item starts with the same letters as the text field value:*/
+            if (index.name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + index.name.substr(0, val.length) + "</strong>";
+                b.innerHTML += index.name.substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + index.id + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function (e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = index.name;
+                    inp.setAttribute('data-id', index.id);
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        })
+    });
+
+    // execute a function presses a key on the keyboardF
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+/**
+ * switch form initialize
+ */
+async function switchFormInit(roomId) {
+    let rooms = await RoomProvider.getAll();
+    const data = rooms.sort((a, b) => a.name - b.name).filter(item => item.id !== roomId);
+
+    autocomplete(document.getElementById("switch-form-to"), data);
 }
